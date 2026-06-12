a1d54b62b47f79b0022f69b6139dd141e9b0a50c
DENSL <dyunayuna90@gmail.com>
2026-06-12 19:38:01 +0700

    Update APP_VERSION to 2.1.0

--- www/js/config.js
+++ www/js/config.js
@@ -1,4 +1,4 @@
-window.APP_VERSION = "2.0.9";
+window.APP_VERSION = "2.1.0";
 window.UPDATE_URL = "https://raw.githubusercontent.com/dyunayuna90-bit/baca./main/package.json";
 window.RELEASES_URL = "https://github.com/dyunayuna90-bit/baca./releases/latest";
 
